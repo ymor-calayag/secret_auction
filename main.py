313109b3e2f8f4ba3feb85fe5467da44d9702528
ymor-calayag <jeffreyymor.calayag@gmail.com>
2025-12-13 16:25:45 +0800

    Complete bidding program logic
    - Collect user input
    - Store bids in a dictionary
    - Handle additional bids
    - Determine highest bidder

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-# TODO-1: Ask the user for input
-# TODO-2: Save data into dictionary {name: price}
-# TODO-3: Whether if new bids need to be added
-# TODO-4: Compare bids in dictionary
+auction_bids = {}
+highest_bid = 0
+highest_bidder = ""
+
+while True:
+    user_name = input("Participant name: ")
+    user_bid = int(input("Input your bid: $"))
+    add_bidders = input("Are there any other bidders? Type 'yes' or 'no'\n").lower()
+    
+    auction_bids[user_name] = user_bid
+    
+    if add_bidders == "no":
+        top_bidder = max(auction_bids, key=auction_bids.get)
+        print(f"Highest bidder is {top_bidder} with a bid of ${auction_bids[top_bidder]}.")
+        break
+    else:
+        print("\n" * 100)
